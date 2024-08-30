6cf15b025407585294568efc4080727b11f2b9da
hannesblaman <hannes@blaman.com>
2024-08-30 09:54:22 +0200

    Grundläggande objektorientering

---
+++ SY22_PRRPRR02_04_Objektorientering/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY22_PRRPRR02_04_Objektorientering {
+	internal class Circle {
+		int radius;
+		double x;
+		double y;
+		string color;
+		string name;
+
+		public Circle(int inputRadius,
+			   double inputX,
+			   double inputY,
+			   string inputColor,
+			   string inputName) {
+
+			radius = inputRadius;
+			x = inputX;
+			y = inputY;
+			color = inputColor;
+			name = inputName;
+
+		}
+
+		public void DrawSelf() {
+			Console.WriteLine("Hej! Jag är en " + color 
+				+ " cirkel och mitt namn är " + name);
+			Console.WriteLine("radie: " + radius);
+			Console.WriteLine("position: " + x + ", " + y);
+		}
+
+		public double DistanceFromOrigin() {
+			return Math.Abs(x) + Math.Abs(y);
+		}
+
+		public void DoubleSize() {
+			radius *= 2;
+		}
+
+		public void IncreaseSize(int increase) {
+			radius += increase;
+		}
+	}
+}
--- SY22_PRRPRR02_04_Objektorientering/Program.cs
+++ SY22_PRRPRR02_04_Objektorientering/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_04_Objektorientering {
 	internal class Program {
 		static void Main(string[] args) {
+			Circle circle1 = new Circle(5, 2.3, 5, "blue", "Josef");
+			Circle circle2 = new Circle(17, 10.5, 5.5, "red", "Martin");
+
+			Console.WriteLine("Före förändring:");
+			circle1.DrawSelf();
+			circle2.DrawSelf();
+
+			circle1.IncreaseSize(10);
+			circle2.DoubleSize();
+
+			Console.WriteLine("Efter förändring:");
+			circle1.DrawSelf();
+			circle2.DrawSelf();
 		}
 	}
 }
